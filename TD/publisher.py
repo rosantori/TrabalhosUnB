a24c7fd515bebca65b4c58ce5372e880a77f61a0
rosantori <39715463+rosantori@users.noreply.github.com>
2019-12-01 11:40:46 -0300

    Create publisher.py

---
+++ TD/publisher.py
@@ -0,0 +1,16 @@
+import paho.mqtt.client as mqtt
+import time
+
+broker_host =  "192.168.0.169" #default
+broker_port = 1883 #default
+
+client = mqtt.Client()
+client.connect(broker_host, broker_port)
+f = open("data.txt")
+while True:
+    msg = f.readline()
+    if not msg: break
+    client.publish(topic = "UMIDADE", payload = msg[10:15], qos = 0, retain = False)
+    client.publish(topic = "TEMPERATURA", payload = msg[31:36], qos = 0, retain = False)
+    time.sleep(0.01)
+f.close()
