d177db8d6b69ad98bd037c1d18fdad0bdaf0f4d9
rosantori <39715463+rosantori@users.noreply.github.com>
2019-12-01 11:39:08 -0300

    Create subscriber1.py

---
+++ TD/subscriber1.py
@@ -0,0 +1,21 @@
+import socket
+import paho.mqtt.client as mqtt 
+
+HOST = "192.168.0.169"            # Endereco IP do Servidor
+PORT = 1883            # Porta que o Servidor esta
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+orig = (HOST, PORT)
+s.bind(orig)
+s.listen(1)
+while True:
+    con, cliente = s.accept()
+    print ("Conectado por", cliente)
+    while True:
+        msg = con.recv(1024)
+        if not msg: break
+        print (msg)
+        
+        
+    print ('Finalizando conexao do cliente', cliente)
+    con.close()
